85651b894bfc48c6486865d68919c4e04476b61b
Viktor <mr.verkhovsky@gmail.com>
2022-01-24 00:08:49 +0300

    P in Procfile

---
+++ staticfiles/englishmap/layout.0905c1479885.js
@@ -0,0 +1,202 @@
+const topics = [
+    { 
+    id: 1,
+    topic: 'Alphabet', 
+    short: 'a b c d e f g h i j k l m n o p q r s t u v w x y z', 
+    full: 'A: an apple, anacorn, an ant' 
+    },
+
+    { 
+    id: 2,
+    topic: 'Phonemes', 
+    short: 'ch- cheese, b - ball, s - student, sh- sheep, etc.', 
+    full: 'https://www.softschools.com/examples/grammar/phonemes_examples/643/'
+    },
+    { 
+    id: 3,
+    topic: 'Noun', 
+    short: '- Dog, apple, cat, house + I, you, he, she, it, we, they', 
+    full: `A noun is a word that refers to a thing (book),
+    a person (Betty Crocker), an animal (cat), a place (Omaha), 
+    a quality (softness), an idea (justice), or an action (yodeling). 
+    It's usually a single word, but not always: cake, shoes, school bus, 
+    and time and a half are all nouns.`
+    },
+    {
+    id: 4,
+    topic: 'Pronoun', 
+    short: 'I, you, he, she, it, we, they', 
+    full: 'I, you, he, she, it, we, they'
+    },
+    {
+    id: 5,
+    topic: 'Numbers', 
+    short: '1 2 3 4 5 ...', 
+    full: '1 - one, 2 - two, etc.'
+    },
+    {
+    id: 6,
+    topic: 'Adjective', 
+    short: 'Beautiful, tasty', 
+    full: `Adjectives are words that describe the qualities or states of being of nouns: 
+    enormous, doglike, silly, yellow, fun, fast. They can also describe the quantity of nouns: 
+    many, few, millions, eleven`
+    },
+    {
+    id: 7,
+    topic: 'Singular and plural nouns', 
+    short: 'One dog, two dogs', 
+    full: 'Add -s/es to plural nouns'
+    },
+    {
+    id: 8,
+    topic: 'Adjective + Noun', 
+    short: 'Beautiful day, fat dog', 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 9,
+    topic: 'to be (present)', 
+    short: 'Am, is, are => I am, you are, he is, etc.', 
+    full: `We are not, We aren’t, We're not
+    You are not, You aren’t, You're not
+    They are not, They aren't, They're not`
+    },
+    {
+    id: 10,
+    topic: 'To be + adjective/noun', 
+    short: 'I am fat, He is a student, We are strong', 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 11,
+    topic: 'Action verb', 
+    short: 'Go, eat, drive, etc.', 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 12,
+    topic: 'Present simple', 
+    short: 'I swim, He goes to school, she cooks meth', 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 13,
+    topic: 'Present Simple negative', 
+    short: "I don't like tea, She doesn't eat meat", 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 14,
+    topic: 'Present Simple questions', 
+    short: 'Do you know math? Does he drive?', 
+    full: 'Sample text Smaple text'
+    },
+    {
+    id: 15,
+    topic: 'Sentence parts and Sentence structure', 
+    short: 'Subject, helping verb, verb, object', 
+    full: `Subject => noun, Helping verb => to be: am/is/are/was/were/been,
+    do, did, + modal verbs`
+    },
+    {
+    id: 16,
+    topic: 'Sample card', 
+    short: 'Hi there!', 
+    full: 'What up dude'
+    },
+  ];
+  const setLayout = [
+    {
+    rowID: 1, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: '', 
+    topL: 0,
+    topC: 1,
+    topR: 0,
+    },
+    {
+    rowID: 2, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: '', 
+    topL: 0,
+    topC: 2,
+    topR: 0,
+    },
+    {
+    rowID: 3, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: '', 
+    topL: 0,
+    topC: 3,
+    topR: 0,
+    },
+    {
+    rowID: 4, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowR', 
+    topL: 0,
+    topC: 4,
+    topR: 5,
+    },
+    {
+    rowID: 5, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowL', 
+    topL: 6,
+    topC: 7,
+    topR: 0,
+    },
+    {
+    rowID: 6, 
+    arrL: 'arrowR',
+    arrC: 'arrowC', 
+    arrR: '', 
+    topL: 0,
+    topC: 8,
+    topR: 9,
+    },
+    {
+    rowID: 7, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowC', 
+    topL: 0,
+    topC: 10,
+    topR: 11,
+    },
+    {
+    rowID: 8, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowL', 
+    topL: 0,
+    topC: 12,
+    topR: 0,
+    },
+    {
+    rowID: 9, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowR', 
+    topL: 0,
+    topC: 13,
+    topR: 14,
+    },
+    {
+    rowID: 10, 
+    arrL: '',
+    arrC: 'arrowC', 
+    arrR: 'arrowC', 
+    topL: 0,
+    topC: 15,
+    topR: 16,
+    },
+    
+
+  ];
